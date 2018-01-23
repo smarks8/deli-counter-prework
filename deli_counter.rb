e1472c15201e46d81110a4d81c52694f84777719
Stacey  Marks <staceyamarks@gmail.com>
2018-01-23 16:49:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(line)
   if line.length == 0 
     puts "The line is currently empty."
   else
+    current_line = "The line is currently: "
     
-    line.map.with_index {|name, index| "The line is currently: #{index+1}. #{name} "}
+    line.map.with_index {|name, index|  #{index+1}. #{name} "}
 end
