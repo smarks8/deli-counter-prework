19c596d2938efa84172d2b9ad6beb0c3a468fb51
Stacey  Marks <staceyamarks@gmail.com>
2018-01-23 16:58:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(line)
   if line.length == 0 
     puts "The line is currently empty."
   else 
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     line.each.with_index do |name, index|
-      current_line << "#{index+1}. #{name} "
+      current_line << " #{index+1}. #{name}"
     end
     puts current_line
   end
