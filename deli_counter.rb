a6a92ae073e9b6c2785e0c188d26d4e2c93975e1
Stacey  Marks <staceyamarks@gmail.com>
2018-01-23 17:02:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ end
 def take_a_number(line, name)
   line.push(name)
   
-  "Welcome, #{name}. You are number #{line.length} in line."
+  puts "Welcome, #{name}. You are number #{line.length} in line."
 end 
